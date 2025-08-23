f5ab5509b885458406a5a637c384b04201b9e2d3
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-23 20:14:59 +0500

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Footer from '@/components/footer';
+import footer from '@/components/footer';
+
+export default function Page() {
+  return (
+    <div className="min-h-screen bg-white">
+      <main>
+        <Footer isDarkMode={false} />
+      </main>
+      
+    </div>
+  )
+}
