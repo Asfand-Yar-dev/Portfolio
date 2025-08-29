8e4ef923a4fcd5a205b9f9e178b49d63fe2e8ea3
Asfand-Yar-dev <asfandyar273263@gmail.com>
2025-08-29 23:22:18 +0500

    Refactor Page component to implement dark mode and update Footer, About, and Contact components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-import Footer from '@/components/footer';
-import footer from '@/components/footer';
+"use client"
+
+import Footer from '@/components/Footer';
+import About from '@/components/About';
+import Contact from '@/components/Contact';
+import { useState } from 'react';
 
 export default function Page() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <div className="min-h-screen bg-white">
       <main>
-        <Footer isDarkMode={false} />
+
+      <About isDarkMode={isDarkMode} />
+      {/* Divider */}
+      <div className={`w-full h-[3px] ${isDarkMode ? 'bg-gray-800' : 'bg-gray-200'}`} />
+
+       <Contact isDarkMode={isDarkMode} />
+      {/* Divider */}
+      <div className={`w-full h-[3px] ${isDarkMode ? 'bg-gray-800' : 'bg-gray-200'}`} />
+
+      {/* Divider */}
+      <Footer isDarkMode={isDarkMode} />
+      <div className={`w-full h-[3px] ${isDarkMode ? 'bg-gray-800' : 'bg-gray-200'}`} />
+
       </main>
       
     </div>
